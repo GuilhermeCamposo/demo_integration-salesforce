cef517ab5c52893aff7e366224041ae139240bcf
Guilherme Camposo <guilherme.camposo@me.com>
2021-11-04 14:46:12 -0300

    adding salesforce logic

--- src/main/java/integration/demo/APIRoute.java
+++ src/main/java/integration/demo/APIRoute.java
@@ -1,19 +1,41 @@
 package integration.demo;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 
 public class APIRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
-      rest("/v1/ingestor")
-      .post()
-      .to("direct:postRoute");
+        restConfiguration().bindingMode(RestBindingMode.json);
 
-
-      from("direct:postRoute")
-      .log("received body: ${body} ");
+        rest("/opportunities")
+                .post()
+                .consumes("application/json")
+                .route()
+                .routeId("opportunityPost")
+                .setProperty("oppWebhook", simple("${body}"))
+                .setProperty("accountId", jsonpath("$.new[0].AccountId"))
+                .setProperty("opportunityId", jsonpath("$.new[0].Id"))
+                //GET Account Info
+                .removeHeaders("*")
+                .setBody(constant(""))
+                .setHeader("Authorization", simple("{{sf.accessToken}}"))
+                .setHeader(Exchange.HTTP_QUERY, simple("q={{sf.account.query}}='${exchangeProperty.accountId}'"))
+                .to("vertx-http:{{sf.host}}?httpMethod=GET")
+                .setProperty("accountInfo", simple("${body}"))
+                //GET line Items Info
+                .removeHeaders("*")
+                .setBody(constant(""))
+                .setHeader("Authorization", simple("{{sf.accessToken}}"))
+                .setHeader(Exchange.HTTP_QUERY, simple("q={{sf.lineItems.query}}='${exchangeProperty.opportunityId}'"))
+                .to("vertx-http:{{sf.host}}?httpMethod=GET")
+                .setProperty("lineItemsInfo", simple("${body}"))
+                //TODO Aggregate
+                .setBody(simple("webhook :: ${exchangeProperty.oppWebhook} , accountInfo :: ${exchangeProperty.accountInfo}, ItemLinesInfo :: ${exchangeProperty.lineItemsInfo}"))
+                .endRest();
 
     }
 
