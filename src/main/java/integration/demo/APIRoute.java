0d03efc6479147ad492fb47bc406e37e1120ba3c
Guilherme Camposo <guilherme.camposo@me.com>
2021-11-10 18:48:22 -0300

    adding  salesforce component

--- src/main/java/integration/demo/APIRoute.java
+++ src/main/java/integration/demo/APIRoute.java
@@ -1,44 +1,52 @@
 package integration.demo;
 
+
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 
 public class APIRoute extends RouteBuilder {
 
-    @Override
-    public void configure() throws Exception {
 
-        restConfiguration().bindingMode(RestBindingMode.json);
+    @Override
+    public void configure() throws Exception{
+
+        restConfiguration().bindingMode(RestBindingMode.off);
 
         rest("/opportunities")
-                .post()
+            .post()
                 .consumes("application/json")
+                .produces("application/json")
                 .route()
-                .routeId("opportunityPost")
-                .setProperty("oppWebhook", simple("${body}"))
-                .setProperty("accountId", jsonpath("$.new[0].AccountId"))
-                .setProperty("opportunityId", jsonpath("$.new[0].Id"))
-                //GET Account Info
-                .removeHeaders("*")
-                .setBody(constant(""))
-                .setHeader("Authorization", simple("{{sf.accessToken}}"))
-                .setHeader(Exchange.HTTP_QUERY, simple("q={{sf.account.query}}='${exchangeProperty.accountId}'"))
-                .to("vertx-http:{{sf.host}}?httpMethod=GET")
-                .setProperty("accountInfo", simple("${body}"))
-                //GET line Items Info
-                .removeHeaders("*")
-                .setBody(constant(""))
-                .setHeader("Authorization", simple("{{sf.accessToken}}"))
-                .setHeader(Exchange.HTTP_QUERY, simple("q={{sf.lineItems.query}}='${exchangeProperty.opportunityId}'"))
-                .to("vertx-http:{{sf.host}}?httpMethod=GET")
-                .setProperty("lineItemsInfo", simple("${body}"))
-                //TODO Aggregate
-                .setBody(simple("webhook :: ${exchangeProperty.oppWebhook} , accountInfo :: ${exchangeProperty.accountInfo}, ItemLinesInfo :: ${exchangeProperty.lineItemsInfo}"))
-                .endRest();
+                    .routeId("opportunityPost")
+                    .setProperty("oppWebhook", simple("${body}"))
+                    .setProperty("accountId", jsonpath("$.new[0].AccountId"))
+                    .setProperty("opportunityId", jsonpath("$.new[0].Id"))
+                    //GET Account Info
+                    .removeHeaders("*")
+                    .setBody(constant(""))
+                    .setHeader("q", simple("{{sf.query.account}}='${exchangeProperty.accountId}'"))
+                    .to("salesforce:raw?format=JSON&rawMethod=GET&rawQueryParameters=q&rawPath={{sf.url.path}}")
+                    .setProperty("accountInfo").jsonpathWriteAsString("$.records")
+                    //TODO remove this workaround
+                    .process(new Processor() {
+                        @Override
+                        public void process(Exchange exchange) throws Exception {
+                            exchange.setProperty("accountInfo", exchange.getProperty("accountInfo", String.class).replace("[", "").replace("]", "") );
+                        }
+                    })
+                    //GET line Items Info
+                    .removeHeaders("*")
+                    .setBody(constant(""))
+                    .setHeader("q", simple("{{sf.query.lineItems}}='${exchangeProperty.opportunityId}'"))
+                    .to("salesforce:raw?format=JSON&rawMethod=GET&rawQueryParameters=q&rawPath={{sf.url.path}}")
+                    .setProperty("lineItemsInfo").jsonpathWriteAsString("$.records")
+                    .setBody(simple(" { \"opportunity\" : ${exchangeProperty.oppWebhook}, \"itemLines\" : ${exchangeProperty.lineItemsInfo},  \"account\" : ${exchangeProperty.accountInfo} }"))
+        .endRest();
 
     }
 
-  
+
 
 }
---
+++ src/main/java/integration/demo/Configurations.java
@@ -0,0 +1,43 @@
+package integration.demo;
+
+import org.apache.camel.component.salesforce.SalesforceComponent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@ApplicationScoped
+public class Configurations {
+
+    @ConfigProperty(name = "sf.clientId")
+    String clientId;
+
+    @ConfigProperty(name = "sf.clientSecret")
+    String clientSecret;
+
+    @ConfigProperty(name = "sf.username")
+    String username;
+
+    @ConfigProperty(name = "sf.password")
+    String password;
+
+    @ConfigProperty(name = "sf.instance")
+    String instance;
+
+    @ConfigProperty(name = "sf.login.url")
+    String loginURL;
+
+    @Named
+    SalesforceComponent salesforce(){
+
+        SalesforceComponent component = new SalesforceComponent();
+        component.setClientId(clientId);
+        component.setClientSecret(clientSecret);
+        component.setUserName(username);
+        component.setPassword(password);
+        component.setInstanceUrl(instance);
+        component.setLoginUrl(loginURL);
+
+        return component;
+    }
+
+}
