5e676aad73014123fe5416d8066d62da0c6687e1
Guilherme Camposo <guilherme.camposo@me.com>
2022-07-05 15:42:06 -0400

    Adding atlasmap as the mapping tool

--- src/main/java/integration/demo/APIRoute.java
+++ src/main/java/integration/demo/APIRoute.java
@@ -2,11 +2,18 @@ package integration.demo;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.jsonpath.JsonPathExpression;
 import org.apache.camel.model.rest.RestBindingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class APIRoute extends RouteBuilder {
+
+    private static final String TRIGGER_ID = "trigger-request-0e931a24-3a52-42f8-9f5d-3bb6a4cf4add";
+    private static final String ACCOUNT_ID = "account-9494ce35-b5c9-4215-b2db-e97c98e2f2cf";
+    private static final String LINE_ITEMS_ID = "line-items-65e7bec8-01e3-49e6-be8e-cfbf991a9191";
 
     @Override
     public void configure() throws Exception{
@@ -28,7 +35,9 @@ public class APIRoute extends RouteBuilder {
                 .consumes("application/json")
                 .route()
                     .routeId("opportunityPost")
-                    .setProperty("oppWebhook", simple("${body}"))
+                    .convertBodyTo(String.class)
+                    .setProperty("trigger", simple("${body}"))
+                    .log("Trigger  -> ${exchangeProperty.trigger}")
                     .setProperty("accountId", jsonpath("$.new[0].AccountId"))
                     .setProperty("opportunityId", jsonpath("$.new[0].Id"))
                     //GET Account Info
@@ -36,17 +45,29 @@ public class APIRoute extends RouteBuilder {
                     .setBody(constant(""))
                     .setHeader("q", simple("{{sf.query.account}}='${exchangeProperty.accountId}'"))
                     .to("salesforce:raw?format=JSON&rawMethod=GET&rawQueryParameters=q&rawPath={{sf.url.path}}")
-                    .setProperty("accountInfo", jpl)
-                    .log("AccountInfo -> ${exchangeProperty.accountInfo} ")
+                    .setProperty("account", jpl)
+                    .log("Account  -> ${exchangeProperty.account}")
                     //GET line Items Info
                     .removeHeaders("*")
                     .setBody(constant(""))
                     .setHeader("q", simple("{{sf.query.lineItems}}='${exchangeProperty.opportunityId}'"))
                     .to("salesforce:raw?format=JSON&rawMethod=GET&rawQueryParameters=q&rawPath={{sf.url.path}}")
-                    .setProperty("lineItemsInfo").jsonpathWriteAsString("$.records")
+                    .setProperty("line-items", jpl)
+                    .log("Line Items  -> ${exchangeProperty.account}")
                     //POST enriched request to Asana Adapter
                     .to("micrometer:counter:get_account_details")
-                    .setBody(simple(" { \"opportunity\" : ${exchangeProperty.oppWebhook}, \"lineItems\" : ${exchangeProperty.lineItemsInfo},  \"account\" : ${exchangeProperty.accountInfo} }"))
+                    .process(new Processor() {
+                        @Override
+                        public void process(Exchange exchange) throws Exception {
+                            Map<String, Object> docs = new HashMap<String, Object> ();
+                            docs.put(TRIGGER_ID, exchange.getProperty("trigger"));
+                            docs.put(ACCOUNT_ID, exchange.getProperty("account"));
+                            docs.put(LINE_ITEMS_ID, exchange.getProperty("line-items"));
+
+                            exchange.getIn().setBody(docs);
+                        }
+                    })
+                    .to("atlasmap:atlasmap-mapping.adm")
                     .log("Sending -> ${body}")
                     .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
                     .to("{{asana.adapter.url}}/asanaAdapter")
